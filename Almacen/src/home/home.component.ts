551c4de85309fe2e24f8622054d1e7981de90ed9
chaparro-dom <juancaboltwolverine680@gmail.com>
2024-10-16 13:24:55 -0600

    Add files via upload

---
+++ Almacen/src/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  // Aquí podrías agregar lógica para interactuar con el sidebar si es necesario
+}
